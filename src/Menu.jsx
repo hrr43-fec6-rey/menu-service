f1de8ee3b0e497e1187318d4b9e4f25f9ce1c8ed
Tom Nicholas <tom_here@yahoo.com>
2020-01-25 19:08:24 -0800

    modified Menu and MenuNav

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -2,19 +2,22 @@ const React = require('react');
 const $ = require('jquery');
 import css from './Menu.css';
 import MenuNav from './MenuNav.jsx';
+import MenuMenu from './MenuMenu.jsx';
 
 class Menu extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      menus: []
+      menus: null,
+      menuSelected: 0
     };
+    this.selectMenu = this.selectMenu.bind(this);
   }
 
   componentDidMount() {
     console.log('requesting')
     $.ajax({
-      url: '/getmenu/1',
+      url: '/getmenu/2',
       success: (restaurant) => {
         this.setState({menus: restaurant[0].menus});
         console.log('menus = ' + restaurant[0].menus.length);
@@ -25,11 +28,22 @@ class Menu extends React.Component {
     });
   }
 
+  selectMenu(index) {
+    var temp = this.state.menus[index];
+    this.setState({
+      menuSelected: index
+    });
+  }
+
   render() {
     return (
       <div className="menu-main">
-        <h1>Menu</h1>
-        <MenuNav menus={this.state.menus} />
+        <div className="menu-title">Menu</div>
+        <MenuNav
+          menus={this.state.menus}
+          menuSelected={this.state.menuSelected}
+          action={this.selectMenu}
+        />
       </div>
     );
   }
--- src/MenuNav.jsx
+++ src/MenuNav.jsx
@@ -1,15 +1,70 @@
 const React = require('react');
 
-const MenuNav = ({menus}) => {
-  return (
-    <div className="menu-nav">
-      {menus.map(menu =>
-        <div className="menu-nav-item" key={menu.title}>
-          {menu.title}
+class MenuNav extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(index) {
+    this.props.action(index);
+  }
+
+  render() {
+    if (!this.props.menus) return null;
+    return (
+      <div className="menu-and-nav">
+        <div className="menu-nav">
+          {this.props.menus.map((menu, index) =>
+            <div
+              className={'menu-nav-item' + (index === this.props.menuSelected ? ' menu-nav-item-selected':'')}
+              key={index}
+              onClick={this.handleClick.bind(this, index)}
+            >
+              {menu.title}
+            </div>
+          )}
         </div>
-      )}
-    </div>
-  );
+        <div className="menu-menu-description">
+          {this.props.menus[this.props.menuSelected].description}
+        </div>
+
+        {this.props.menus[this.props.menuSelected].sections.map((section, index) =>
+        <div className="menu-section" key={index}>
+          <div className="menu-section-title">
+            {section.title}
+          </div>
+          <div className="menu-items">
+            {section.items.map((item, index) =>
+              <div className="menu-item" key={index}>
+                <div className="menu-item-title">
+                  <div className="menu-item-title-name">
+                    {item.title}
+                  </div>
+                  <div className="menu-item-title-price">
+                    {item.price}
+                  </div>
+                </div>
+                <div className="menu-item-description">
+                  {item.description}
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+        )}
+        <div className="menu-collapse">
+          <div className="menu-collapse1"></div>
+          <div className="menu-collapse2">
+            <div className="container1">
+              <div className="menu-view">View full menu</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default MenuNav;
