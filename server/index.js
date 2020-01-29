9e69055e5b5b60bc8d331ecf865ae43e7b6dc4bb
Tom <tomhere@gmail.com>
2020-01-28 17:08:44 -0800

    please review Title.jsx ... thanks

--- server/index.js
+++ server/index.js
@@ -4,18 +4,26 @@ const db = require('./../database');
 
 const app = express();
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/getmenu/:id', (req, res) => {
   console.log(`requesting ${req.params.id}`);
   db.getRestaurantMenu(req.params.id, (restaurant) => {
-    res.status(200).send(restaurant);
+    res.status(200).json(restaurant);
   });
 });
 
+app.get('/gettitle/:id', (req, res) => {
+  console.log(`requesting ${req.params.id}`);
+  db.getRestaurantTitle(req.params.id, (restaurant) => {
+    res.status(200).json(restaurant);
+  });
+});
 
-app.listen(process.env.PORT, () => {
+const port = (process.env.PORT ? process.env.PORT : 8000);
+
+app.listen(port, () => {
   console.log(`App listening on port ${process.env.PORT}`);
 });
---
+++ src/Title.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+function Title(props) {
+  const [restaurantName, setRestaurantName] = useState('Loading....');
+
+  useEffect(() => {
+    const { id } = props;
+    console.log(`requesting ${id}`);
+
+    fetch(`/gettitle/${id}`)
+      .then((response) => response.json())
+      .then((myJson) => {
+        setRestaurantName(myJson);
+        console.log(`title = ${myJson}`);
+      });
+  }, []);
+
+  return (
+    <div className="restaurant-title">
+      {restaurantName}
+    </div>
+  );
+}
+
+Title.defaultProps = {
+  id: '1',
+};
+
+Title.propTypes = {
+  id: PropTypes.string,
+};
+
+export default Title;
